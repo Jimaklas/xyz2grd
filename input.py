4fc76d1312273d6a3cdac9d0fc2e73c560e4386b
Jimaklas <Jimaklas@users.noreply.github.com>
2014-11-06 13:28:06 +0200

    Module containing input data for the main program

---
+++ input.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# File MUST be CP1253 encoded and sections in file must be sorted by station (ascending)!!!
+# STA_FILE = u"c:\\Documents and Settings\\user\\Τα έγγραφά μου\\NEW\\Artiria_Diatomes.sta"  # File MUST be CP1253 encoded!!!
+# STA_FILE = u"c:\\Documents and Settings\\user\\Τα έγγραφά μου\\NEW\\SR7D_Diatomes.sta"  # File MUST be CP1253 encoded!!!
+# STA_FILE = u"c:\\Documents and Settings\\user\\Τα έγγραφά μου\\NEW\\SR5A_Diatomes.sta"  # File MUST be CP1253 encoded!!!
+STA_FILE = u"c:\\Documents and Settings\\user\\Τα έγγραφά μου\\NEW\\SR5A_Diatomes_OLES.sta"  # File MUST be CP1253 encoded!!!
+# STA_FILE = u"c:\\Documents and Settings\\user\\Τα έγγραφά μου\\NEW\\SR8D_Diatomes.sta"  # File MUST be CP1253 encoded!!!
+
+XYZ_FILE = "metr.seq"
+GRD_FILE = "T3A.GRD"
